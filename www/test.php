deebc754d49d973d51fc9e6557a644862fa0a333
Léandre <leandre.colard@gmail.com>
2022-12-06 12:05:03 +0100

    Create test.php

---
+++ www/test.php
@@ -0,0 +1,21 @@
+<?php
+include_once __DIR__ . '/../db.php';
+
+$pageTitle = "SLAV E-SHOP";
+
+// Commencer a ecrire dans la memoire tampon
+ob_start();
+?>
+
+    <h1>E SHOP - SLAV LIMITED LTD</h1>
+
+<?php
+// TEST XSS - WHY htmlspecialchars()
+// $testContent = "<h2>Test content</h2>";
+// echo $testContent;
+// echo htmlspecialchars($testContent);
+?>
+
+<?php
+// On arrete d'ecrire dans la memoire tampon et on recupere le contenu precedent
+$pageContent = ob_get_clean();
