bfa92ef5b714b9e26a0134b1de7305421beda576
Léandre <leandre.colard@gmail.com>
2022-12-06 17:03:07 +0100

    tahdaronne

--- php/database.inc.php
+++ php/database.inc.php
@@ -1,5 +1,5 @@
 <?php
- include_once('db-config.php');
+ include_once('./includes/db-config.php');
     try {
         $db = new PDO ('mysql:host=' .$hostname.';dbname='.$conf['nameBDD'].';charset=utf8', $username , $password,[PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
     } 
