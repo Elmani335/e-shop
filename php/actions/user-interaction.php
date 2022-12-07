550f763396153aae3fde6b82b86d3c793a4a895c
Léandre <leandre.colard@gmail.com>
2022-12-07 15:45:16 +0100

    tah

--- php/actions/user-interaction.php
+++ php/actions/user-interaction.php
@@ -6,24 +6,25 @@
       require_once('../init.php');
       require_once('../database.inc.php');
 
-      spl_autoload_register(function($class){
-          require_once('../classes/'.$class.'.php');
-      });
+      #spl_autoload_register(function($class){
+      #    require_once('../classes/'.$class.'.php');
+      #});
 
       require_once('../database.inc.php');
       if (isset($_POST['pseudo']) && isset($_POST['password'])) {
           $pseudo = $_POST['pseudo'];
           $mdp = $_POST['password'];
 
-          $recuperationUtilisateur = $db->prepare('SELECT `password`, `id_user` FROM user WHERE pseudo = :pseudo');
+          $recuperationUtilisateur = $db->prepare('SELECT `password`, `id_user` FROM user WHERE pseudo = $pseudo');
           $recuperationUtilisateur->execute([
-              'password' => $mdp
+              'password' => $password,
+              'pseudo' => $pseudo
           ]);
           $utilisateurRecupere = $recuperationUtilisateur->fetch();
           $mdp_valid = ($mdp == $utilisateurRecupere['password']) ? true : false;
 
           if ($mdp_valid) {
-              $recuperationUtilisateur = $db->prepare('SELECT id_user, pseudo  FROM user WHERE id_user = :id_user ');
+              $recuperationUtilisateur = $db->prepare('SELECT id_user, pseudo  FROM user WHERE id_user = $userid ');
               $recuperationUtilisateur->setFetchMode(PDO::FETCH_CLASS, 'User');
               $recuperationUtilisateur->execute([
                   'id_user' => $utilisateurRecupere['id_user'],
