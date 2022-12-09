f586e78bdc895c2488cd7c951110e85c01767d40
ISMAILOV <elmani3107@gmail.com>
2022-12-09 13:58:54 +0100

    pm
    p

--- php/views/pages/products.php
+++ php/views/pages/products.php
@@ -19,53 +19,21 @@ ob_start();
     <div class="row2">
 
         <?php
-        function get_products() {
-            global $db;
-            $query = 'SELECT * FROM product';
-            $statement = $db->prepare($query);
-            $statement->execute();
-            $products = $statement->fetchAll();
-            $statement->closeCursor();
-            return $products;
-        }
-        $products = get_products();
-        foreach ($products as $product) : ?>
-        <div class="col-md-4">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title
-                        <?php if ($product['stock_product'] == 0) {
-                        echo "out-of-stock";
-                    } ?>">
-                        <?= $product['name_product'] ?>
-                    </h5>
-                    <--! show all product in a table format -->
+function get_products()
+{
+    global $db;
+    $query = 'SELECT * FROM product';
+    $statement = $db->prepare($query);
+    $statement->execute();
+    $products = $statement->fetchAll();
+    $statement->closeCursor();
+    return $products;
+}
+$products = get_products();
+foreach ($products as $product) :
+    // show the products in a table
+    echo $product['name_product'] . " - " . $product['price_product'] . "€" . " - " . $product['description_product'] . "<br>";
 
-                    <p class="card-text">
-                        <table>
-                            <tr>
-                                <td>Price</td>
-                                <td><?= $product['price_product'] ?> €</td>
-                            </tr>
-                            <tr>
-                                <td>Stock</td>
-                                <td><?= $product['stock_product'] ?></td>
-                            </tr>
-                            <tr>
-                                <td>Category</td>
-                                <td><?= $product['category_product'] ?></td>
-                            </tr>
-                            
-                            <tr
-                    <a href="index.php?page=product&id=<?= $product['id_product'] ?>" class="btn btn-primary">See more</a>
+?>
 
-                </div>
-            </div>
-        </div>
-        <?php endforeach; ?>
-    </div>
-</div>
-
-
-
-        <?php $pageContent = ob_get_clean(); ?>
+        <?php $pageContent = ob_get_clean() ?>
