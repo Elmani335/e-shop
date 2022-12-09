b6beb535c2b3a9d076fff0e5a20874ea95c3340b
Léandre <leandre.colard@gmail.com>
2022-12-09 14:36:08 +0100

    Update products.php

--- php/views/pages/products.php
+++ php/views/pages/products.php
@@ -22,7 +22,7 @@ ob_start();
 function get_products()
 {
     global $db;
-    $query = 'SELECT * FROM product';
+    $query = 'SELECT * FROM product INNER JOIN join_categorie.id_categorie on product.nom_categorie';
     $statement = $db->prepare($query);
     $statement->execute();
     $products = $statement->fetchAll();
