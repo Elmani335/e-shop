bdd9e714326e18150f9e17aae654949d017b5340
Léandre <44951955+leandre0@users.noreply.github.com>
2022-12-09 11:28:47 +0100

    Update user-interaction.php
    modif product for admin user

--- www/actions/user-interaction.php
+++ www/actions/user-interaction.php
@@ -2,6 +2,30 @@
 require_once __DIR__ . '/../../php/init.php';
 #require_once('../includes/db-config.php');
 
+function admin_add_product($product, $productName, $productCateg, $productDesc, $productPrice, $productStock){
+    global $IsUserConnected;
+    if ($IsUserConnected && ){
+
+    }
+}
+
+function set_error($error){
+    $_SESSION['error'] = $error;
+}
+
+
+global $IsUserConnected;
+if ($IsUserConnected == true && isset($_POST['logout'])) {
+    function logout()
+    {
+        session_start();
+        if (isset($_SESSION['user'])) {
+            session_destroy();
+        }
+        header('Location: ../index.php');
+    }
+}
+
 if (isset($_POST['login'])) {
     echo "\n login";
     echo "<br>";
@@ -43,23 +67,6 @@ if (isset($_POST['login'])) {
         }
 }
 
-function set_error($error)
-{
-    $_SESSION['error'] = $error;
-}
-
-global $IsUserConnected;
-if ($IsUserConnected == true && isset($_POST['logout'])) {
-    function logout()
-    {
-        session_start();
-        if (isset($_SESSION['user'])) {
-            session_destroy();
-        }
-        header('Location: ../index.php');
-    }
-}
-
 // register user, hash password, insert into db, redirect to login page, make sure to check if user already exists, if so, redirect to register page with error message, check is both password fields match, if not, redirect to register page with error message
 
 if (isset($_POST['register'])) {
@@ -84,5 +91,3 @@ if (isset($_POST['register'])) {
             }
 }
 
-
-?>
